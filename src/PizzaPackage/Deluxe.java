3e95122d98d58e66d18362f728338de55df0c12f
tinorterry <58237976+tinorterry@users.noreply.github.com>
2020-04-08 13:50:14 -0400

    Update Deluxe.java

--- src/PizzaPackage/Deluxe.java
+++ src/PizzaPackage/Deluxe.java
@@ -8,6 +8,8 @@ package PizzaPackage;
  * @author Tin Fung
  */
 public class Deluxe extends Pizza {
+    private final int Deluxe_small=14;
+
     // !!! NEEDS TO BE COMPLETED
     // NOTE THAT THE PROTECTED VARIABLES ARE AVAILABLE HERE SO DON'T NEED TO RE-CREATE THEM HERE
 
@@ -31,9 +33,18 @@ public class Deluxe extends Pizza {
      */
     @Override
     public int pizzaPrice() {
-        // !!! NEEDS TO BE COMPLETED
-        return 0;
+
+        if(this.size.equals("Small")){
+            return Deluxe_small;
+        }else  if(this.size.equals("Medium")){
+            return Deluxe_small+2;
+        }else if(this.size.equals("Large")){
+            return Deluxe_small+4;
+        }
+            // !!! NEEDS TO BE COMPLETED
+            return 0;
     }
+        // !!! NEEDS TO BE COMPLETED
 
 
     /**
@@ -44,7 +55,15 @@ public class Deluxe extends Pizza {
      */
     @Override
     public String toString() {
-        // !!! NEEDS TO BE COMPLETED
-        return "";
+        return this.style+" pizza with a "+this.size+" size and the prize is $"+this.pizzaPrice()+" dollars.";
+
+    }
+    public static void main(String[] args){
+         Deluxe T1=new Deluxe("Deluxe","Small");
+         System.out.println(T1.toString());
+        Deluxe T2=new Deluxe("Deluxe","Medium");
+        System.out.println(T2.toString());
+        Deluxe T3=new Deluxe("Deluxe","Large");
+        System.out.println(T3.toString());
     }
 }
